aca74a9838b6f6cc13073a7b227bbfd1c0ddeae5
Nicholas Merritt <nimerritt@gmail.com>
2017-06-06 17:45:21 +0200

    fix git-completion and add colors

--- scripts/configure_git.sh
+++ scripts/configure_git.sh
@@ -3,5 +3,11 @@ git config --global user.email nimerritt@gmail.com
 
 # Configure auto-complete and promt
 # See https://wiki.archlinux.org/index.php/git#Bash_completion
-echo 'source /usr/share/git/completion/git-compleition.sh' >> ~/.bashrc
-echo 'source /usr/share/git/completion/git-prompt.sh' >> ~/.bashrc
+cat <<EOF >> ~/.bashrc
+source /usr/share/git/completion/git-completion.bash
+source /usr/share/git/completion/git-prompt.sh
+export PROMPT_COMMAND='__git_ps1 "\u@\h:\W" "\\\$ ";'
+GIT_PS1_SHOWUPSTREAM="auto"
+GIT_PS1_SHOWCOLORHINTS="yes"
+EOF
+
